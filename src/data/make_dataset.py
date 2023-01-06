737ad53a3acdf3b35350b138e7abfe3b79e55d28
Tolsager <victortolsagerolesen1@gmail.com>
2023-01-06 11:55:00 +0100

    create unit test folder

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -4,6 +4,12 @@ import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
 
+from torch.utils.data import Dataset
+import pandas as pd
+import torch
+import os
+import transformers
+
 
 @click.command()
 @click.argument("input_filepath", type=click.Path(exists=True))
@@ -14,6 +20,33 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
+
+
+def get_tokenizer() -> transformers.AlbertTokenizerFast:
+    path_tokenizers = "tokenizers"
+    tokenizer_type = "albert"
+    path_tokenizer = os.path.join(path_tokenizers, tokenizer_type)
+    if not os.path.exists(path_tokenizer):
+        tokenizer = transformers.AutoTokenizer.from_pretrained(tokenizer_type)
+        tokenizer.save_pretrained(path_tokenizer)
+    else:
+        tokenizer = transformers.AutoTokenizer.from_pretrained(path_tokenizer)
+    return
+
+
+class NewsDataset(Dataset):
+    def __init__(self, df: pd.DataFrame):
+        super().__init__()
+        self.df = df
+        self.tokenizer = transformers
+
+    def __len__(self):
+        return len(self.df)
+
+    def __getitem__(self, index: int) -> dict[str : torch.Tensor]:
+        # load row
+        row = self.df.iloc[0]
+        text = row["text"]
 
 
 if __name__ == "__main__":
