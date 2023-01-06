f04db944f82e3cc9a584052029355c406827c636
Marcrulo <marcus@presutti.eu>
2023-01-06 11:40:38 +0100

    load kaggle data function

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -3,6 +3,8 @@ import click
 import logging
 from pathlib import Path
 from dotenv import find_dotenv, load_dotenv
+import os
+import zipfile
 
 
 @click.command()
@@ -14,6 +16,47 @@ def main(input_filepath, output_filepath):
     """
     logger = logging.getLogger(__name__)
     logger.info("making final data set from raw data")
+
+    # TODO: do checks before loading kaggle
+    # ...
+
+    load_kaggle(input_filepath, output_filepath)
+
+
+def load_kaggle(input_filepath, output_filepath):
+    # Load environment variables
+    project_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+    dotenv_path = os.path.join(project_dir, ".env")
+    load_dotenv(dotenv_path)
+
+    # Check that kaggle API authentication works
+    try:
+        import kaggle
+    except OSError as e:
+        print("Kaggle API error:")
+        print(e)
+
+    api = kaggle.api
+
+    # Download zipped data
+    zip_folder = input_filepath + "/zip_folder"
+    kaggle.api.dataset_download_file(
+        "clmentbisaillon/fake-and-real-news-dataset",
+        "Fake.csv",
+        path=zip_folder,
+    )
+    kaggle.api.dataset_download_file(
+        "clmentbisaillon/fake-and-real-news-dataset",
+        "True.csv",
+        path=zip_folder,
+    )
+
+    # Unzip data
+    unzipped_folder_raw = input_filepath
+    with zipfile.ZipFile(os.path.join(zip_folder, "Fake.csv.zip"), "r") as zip_ref:
+        zip_ref.extractall(unzipped_folder_raw)
+    with zipfile.ZipFile(os.path.join(zip_folder, "True.csv.zip"), "r") as zip_ref:
+        zip_ref.extractall(unzipped_folder_raw)
 
 
 if __name__ == "__main__":
