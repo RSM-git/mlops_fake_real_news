e66368e2e4e16432fde4b39fcc1b11091fe7d0ef
Marcrulo <marcus@presutti.eu>
2023-01-07 16:42:55 +0100

    Hydra functionality + Black formatting is weird

--- src/models/model.py
+++ src/models/model.py
@@ -7,16 +7,40 @@ from transformers import AutoModelForSequenceClassification
 
 
 class FakeNewsClassifier(LightningModule):
-    def __init__(self, model: str = "albert-base-v2", num_classes: int = 2):
+    def __init__(
+        self,
+        model: str = "albert-base-v2",
+        num_classes: int = 2,
+        batch_size: int = 32,
+        lr: int = 2e-5,
+    ):
         super().__init__()
-        self.model = AutoModelForSequenceClassification.from_pretrained(model, num_labels=num_classes)
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            model, num_labels=num_classes
+        )
         self.criterion = nn.CrossEntropyLoss()
         self.training_metrics = nn.ModuleDict(
-            [["accuracy", torchmetrics.Accuracy(task="binary", num_classes=num_classes)]])
+            [
+                [
+                    "accuracy",
+                    torchmetrics.Accuracy(task="binary", num_classes=num_classes),
+                ]
+            ]
+        )
         self.validation_metrics = nn.ModuleDict(
-            [["accuracy", torchmetrics.Accuracy(task="binary", num_classes=num_classes)]])
+            [
+                [
+                    "accuracy",
+                    torchmetrics.Accuracy(task="binary", num_classes=num_classes),
+                ]
+            ]
+        )
+        self.batch_size = batch_size
+        self.lr = lr
 
-    def forward(self, input_ids: torch.Tensor, attention_mask: torch.Tensor) -> torch.Tensor:
+    def forward(
+        self, input_ids: torch.Tensor, attention_mask: torch.Tensor
+    ) -> torch.Tensor:
         return self.model(input_ids=input_ids, attention_mask=attention_mask).logits
 
     def training_step(self, batch: dict, batch_idx: int) -> torch.Tensor:
@@ -25,7 +49,6 @@ class FakeNewsClassifier(LightningModule):
         labels = batch["labels"]
         logits = self(input_ids, attention_mask)
         loss = self.criterion(logits, labels)
-        
 
         return loss
 
@@ -48,13 +71,13 @@ class FakeNewsClassifier(LightningModule):
         return loss
 
     def configure_optimizers(self, lr: float = 2e-5) -> torch.optim.Optimizer:
-        return torch.optim.AdamW(self.parameters(), lr=lr)
+        return torch.optim.AdamW(self.parameters(), lr=self.lr)
 
     def train_dataloader(self) -> torch.utils.data.DataLoader:
-        return DataLoader(self.train_dataset, batch_size=32, shuffle=True)
+        return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
 
     def val_dataloader(self) -> torch.utils.data.DataLoader:
-        return DataLoader(self.val_dataset, batch_size=32, shuffle=True)
+        return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=True)
 
     def test_dataloader(self) -> torch.utils.data.DataLoader:
-        return DataLoader(self.test_dataset, batch_size=32, shuffle=True)
+        return DataLoader(self.test_dataset, batch_size=self.batch_size, shuffle=True)
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,18 +2,38 @@ from pytorch_lightning import Trainer, loggers
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
 import hydra
 
-from mlops_fake_real_news.src.models.model import FakeNewsClassifier
+# from mlops_fake_real_news.src.models.model import FakeNewsClassifier
+from src.models.model import FakeNewsClassifier
 
-# @hydra.main(config_name=)
-model = FakeNewsClassifier("albert-base-v2", 2)
+import random
+import torch
 
-trainer = Trainer(
-    model,
-    callbacks=[
-        ModelCheckpoint("models/", monitor="val_loss", filename="best_model"),
-        EarlyStopping(monitor="val_loss", patience=2),
-    ],
-    logger=loggers.WandbLogger(project="mlops_fake_real_news"),
-)
 
-trainer.fit(model)
+@hydra.main(config_name="training_conf.yaml", config_path="configs")
+def main(cfg):
+
+    batch_size = cfg.hyperparameters.batch_size
+    lr = cfg.hyperparameters.learning_rate
+    seed = cfg.hyperparameters.seed
+
+    random.seed(seed)
+    torch.manual_seed(seed)
+
+    model = FakeNewsClassifier(
+        model="albert-base-v2", num_classes=2, batch_size=batch_size, lr=lr
+    )
+
+    trainer = Trainer(
+        model,
+        callbacks=[
+            ModelCheckpoint("models/", monitor="val_loss", filename="best_model"),
+            EarlyStopping(monitor="val_loss", patience=2),
+        ],
+        logger=loggers.WandbLogger(project="mlops_fake_real_news"),
+    )
+
+    trainer.fit(model)
+
+
+if __name__ == "__main__":
+    main()
