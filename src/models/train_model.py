c2ce8c492ca7bb7f92a5ffce9ac95c298466f906
Marcrulo <marcus@presutti.eu>
2023-01-07 16:17:44 +0100

    added more required packages

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,12 +1,19 @@
 from pytorch_lightning import Trainer, loggers
 from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+import hydra
 
 from mlops_fake_real_news.src.models.model import FakeNewsClassifier
 
+# @hydra.main(config_name=)
 model = FakeNewsClassifier("albert-base-v2", 2)
 
-trainer = Trainer(model, callbacks=[ModelCheckpoint("models/", monitor="val_loss", filename="best_model"),
-                                    EarlyStopping(monitor="val_loss", patience=2)],
-                  logger=loggers.WandbLogger(project="mlops_fake_real_news"))
+trainer = Trainer(
+    model,
+    callbacks=[
+        ModelCheckpoint("models/", monitor="val_loss", filename="best_model"),
+        EarlyStopping(monitor="val_loss", patience=2),
+    ],
+    logger=loggers.WandbLogger(project="mlops_fake_real_news"),
+)
 
 trainer.fit(model)
